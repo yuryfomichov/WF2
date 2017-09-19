4c36a74c7f2cb7afa4e6b39f843ebb572477993c
yuryfomichov <yury.fomichov@gmail.com>
2017-09-19 20:20:38 +0300

    Update start.py

--- src/start.py
+++ src/start.py
@@ -12,13 +12,11 @@ def run():
     })
     network = Train(loader,
                     model_filename="model.pt",
-                    create_new=True,
+                    create_new=False,
                     print_every=1)
     loss_fn = nn.CrossEntropyLoss().type(network.data_type)
-    network.train(loss_fn, optim.Adam(network.model.parameters(), lr=1e-4, weight_decay=1e-3), num_epochs=8)
-    network.train(loss_fn, optim.Adam(network.model.parameters(), lr=1e-5, weight_decay=1e-4), num_epochs=8)
-    network.train(loss_fn, optim.Adam(network.model.parameters(), lr=1e-6, weight_decay=1e-5), num_epochs=8)
-    network.train(loss_fn, optim.Adam(network.model.parameters(), lr=1e-7, weight_decay=1e-6), num_epochs=8)
+    network.train(loss_fn, optim.Adam(network.model.parameters(), lr=1e-7, weight_decay=1e-7), num_epochs=8)
+    network.train(loss_fn, optim.Adam(network.model.parameters(), lr=1e-8, weight_decay=1e-8), num_epochs=8)
 
 
 run()
