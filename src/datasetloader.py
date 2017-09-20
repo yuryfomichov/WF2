63e28137860899e890226cb00deea68511df784c
y.fomichev <yury.fomichov@gmail.com>
2017-09-20 17:28:34 +0300

    updates

--- src/datasetloader.py
+++ src/datasetloader.py
@@ -13,6 +13,7 @@ class DatasetLoader(object):
         self.image_folder = 'posters'
         self.batch_size = params.get("batch_size", 200)
         self.num_workers = params.get("num_workers", 32)
+        self._load_data()
 
     def _get_loader(self,data, drop_last = False, is_train = True):
         imageFolder = '%s/%s/' % (self.data_dir, self.image_folder)
@@ -25,40 +26,65 @@ class DatasetLoader(object):
         return loader
 
     def get_train_loader(self, drop_last = True):
-        return self._get_loader(self.get_data(self.train_file, 0, 0.8), drop_last, True)
+        return self._get_loader(self.train_data, drop_last, True)
 
     def get_val_loader(self):
-        return self._get_loader(self.get_data(self.train_file, 0.8, 1), False, False)
+        return self._get_loader(self.val_data, False, False)
 
     def get_test_loader(self):
-        return self._get_loader(self.get_data(self.self.test_file), False, False)
+        return self._get_loader(self.test_data, False, False)
 
-    def get_data(self, file_name, part_start=0.0, part_end=1.0):
+    def _prepare_data(self, data):
+
+        features = data[:, 0:27].astype(np.float32)
+        images_urls = data[:, 27:28]
+        self.normalize_columns(features, 2)
+        self.normalize_columns(features, 3)
+        self.normalize_columns(features, 4)
+        self.normalize_columns(features, 5)
+
+
+    def prepare_str_column(self, data, column_index, uniq_values):
+        for i, lang in enumerate(uniq_values):
+            data[data[:, column_index] == lang, column_index] = i
+
+    def normalize_columns(self, data, column_index):
+        column = data[:,column_index].astype(np.float32);
+        data[:, column_index] = ((column - np.amin(column))/column.ptp(0)).astype(np.str)
+
+    def _load_data(self):
+        train_file_data = self._read_csv(self.train_file)
+        train_file_data = train_file_data[1:]
+        uniq_countries = np.unique(train_file_data[:, 3])
+        uniq_lang = np.unique(train_file_data[:, 4])
+        uniq_rate = np.unique(train_file_data[:, 5])
+        self.prepare_str_column(train_file_data, 3, uniq_countries)
+        self.prepare_str_column(train_file_data, 4, uniq_lang)
+        self.prepare_str_column(train_file_data, 5, uniq_rate)
+        self.normalize_columns(train_file_data, 2)
+        self.normalize_columns(train_file_data, 3)
+        self.normalize_columns(train_file_data, 4)
+        self.normalize_columns(train_file_data, 5)
+        length = len(train_file_data)
+
+        self.test_data = self.slice_data(train_file_data, 0.9, 1, length)
+        self.train_data = train_file_data[0: round(0.9 * length)]
+        np.random.shuffle(self.train_data)
+        length = len(self.train_data)
+        self.val_data = self.slice_data(self.train_data, 0.85, 1, length)
+        self.train_data = self.slice_data(self.train_data, 0, 0.85, length)
+        pass
+
+    def slice_data(self, data, start, stop, length):
+        data = data[round(start*length): round(stop*length)]
+        features = data[:, 0:27].astype(np.float32)
+        images_urls = data[:, 27:28]
+        return features, images_urls
+
+    def _read_csv(self, file_name):
         data_file = '%s/%s.csv' % (self.data_dir, file_name)
-        data = []
         with open(data_file, 'r') as f:
             reader = csv.reader(f, delimiter=',')
             values = list(reader)
             data = np.array(values)
-            header = data[0:1]
-            data = data[1:data.shape[0]]
-            random.shuffle(data)
-
-        self.prepare_str_conplumn(data, 3)
-        self.prepare_str_conplumn(data, 4)
-        self.prepare_str_conplumn(data, 5)
-        features = data[:, 0:27].astype(np.float32)
-        images_urls = data[:, 27:28]
-        self.normilize_columns(features, 2)
-        self.normilize_columns(features, 3)
-        self.normilize_columns(features, 4)
-        self.normilize_columns(features, 5)
-        return features[1 + round(part_start * len(features)):round(part_end * len(features))], images_urls
-
-    def prepare_str_conplumn(self, data, column_index):
-        uniq_values = np.unique(data[:, column_index])
-        for i, lang in enumerate(uniq_values):
-            data[data[:, column_index] == lang, column_index] = i
-
-    def normilize_columns(self, data, column_index):
-        data[:, column_index] = (data[:, column_index] - np.amin(data[:,column_index]))/ data[:,column_index].ptp(0)
+        return data
