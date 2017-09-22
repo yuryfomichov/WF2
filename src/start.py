3b544ab9b173fa63ce280ab251e55b5607fa8d62
yuryfomichov <yury.fomichov@gmail.com>
2017-09-22 20:12:25 +0300

    Update start.py

--- src/start.py
+++ src/start.py
@@ -67,7 +67,6 @@ def trainModel(network):
     network.train(loss_fn, optim.Adam(network.model.parameters(), lr=1e-3, weight_decay=1e-3), num_epochs=8)
     network.train(loss_fn, optim.Adam(network.model.parameters(), lr=1e-4, weight_decay=1e-3), num_epochs=8)
     network.train(loss_fn, optim.Adam(network.model.parameters(), lr=1e-5, weight_decay=1e-3), num_epochs=8)
-    network.train(loss_fn, optim.Adam(network.model.parameters(), lr=1e-6, weight_decay=1e-3), num_epochs=8)
     return network
 
 
