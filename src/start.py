783a9998756fef56d343be23764e5e638436c08d
yuryfomichov <yury.fomichov@gmail.com>
2017-09-22 19:23:05 +0300

    Update start.py

--- src/start.py
+++ src/start.py
@@ -72,10 +72,10 @@ def trainModel(network):
 
 
 def start():
-    #network1 = trainModel(getNetwork1())
-    #network2 = trainModel(getNetwork2())
-    #network3 = trainModel(getNetwork3())
-    #network4 = trainModel(getNetwork4())
+    network1 = trainModel(getNetwork1())
+    network2 = trainModel(getNetwork2())
+    network3 = trainModel(getNetwork3())
+    network4 = trainModel(getNetwork4())
     pass
 
 
