ab2d8fffa6676fe19f84a40692be3286518505ed
y.fomichev <yury.fomichov@gmail.com>
2017-09-22 17:11:03 +0300

    several models

--- src/start.py
+++ src/start.py
@@ -72,7 +72,7 @@ def trainModel(network):
 
 
 def start():
-    network1 = trainModel(getNetwork1())
+    #network1 = trainModel(getNetwork1())
     network2 = trainModel(getNetwork2())
     network3 = trainModel(getNetwork3())
     network4 = trainModel(getNetwork4())
