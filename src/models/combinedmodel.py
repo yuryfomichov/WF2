cd26337bd5f2539f4c3e53f59590f1e3fcbdc3e3
y.fomichev <yury.fomichov@gmail.com>
2017-09-22 18:24:56 +0300

    several models

--- src/models/combinedmodel.py
+++ src/models/combinedmodel.py
@@ -14,23 +14,23 @@ class CombinedModel(nn.Module):
             nn.BatchNorm2d(32),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(32, 48, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(48),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(48, 64, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(64, 96, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(96),
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(128, 192, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(192),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2)
         )
         # self._require_grad_false()
 
         self.classifier = nn.Sequential(
-            nn.Linear(96 * 10 * 10, 1024),
+            nn.Linear(192 * 10 * 10, 1024),
             nn.BatchNorm1d(1024),
             nn.ReLU(True),
             nn.Linear(1024, num_classes),
@@ -60,7 +60,7 @@ class CombinedModel(nn.Module):
         x = self.classifier(x)
         x1 = self.secondNet(x1)
 
-        y = x*0.3 + x1*0.7
+        y = (x + x1) / 2
 
         return y
 
--- src/start.py
+++ src/start.py
@@ -64,15 +64,15 @@ def getNetwork4(create_new=True):
 
 def trainModel(network):
     loss_fn = nn.CrossEntropyLoss().type(network.data_type)
-    network.train(loss_fn, optim.Adam(network.model.parameters(), lr=1e-2, weight_decay=1e-3), num_epochs=8)
     network.train(loss_fn, optim.Adam(network.model.parameters(), lr=1e-3, weight_decay=1e-3), num_epochs=8)
     network.train(loss_fn, optim.Adam(network.model.parameters(), lr=1e-4, weight_decay=1e-3), num_epochs=8)
     network.train(loss_fn, optim.Adam(network.model.parameters(), lr=1e-5, weight_decay=1e-3), num_epochs=8)
+    network.train(loss_fn, optim.Adam(network.model.parameters(), lr=1e-6, weight_decay=1e-3), num_epochs=8)
     return network
 
 
 def start():
-    # network1 = trainModel(getNetwork1())
+    network1 = trainModel(getNetwork1())
     network2 = trainModel(getNetwork2())
     # network3 = trainModel(getNetwork3())
     network4 = trainModel(getNetwork4())
